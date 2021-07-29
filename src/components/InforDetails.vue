32f0551ec82a003193e5776963d8e489afda4b65
Tianj0o <2576099053@qq.com>
2021-07-29 23:58:05 +0800

    add

---
+++ src/components/InforDetails.vue
@@ -0,0 +1,30 @@
+<template>
+ <div id="InforDetails">
+   <div class="continer">
+     <img src="../assets/avatar.jpg" alt="个人头像">
+   </div>
+ </div>
+</template>
+<script>
+import { defineComponent } from "@vue/runtime-core";
+
+
+
+export default defineComponent({
+  
+})
+</script>
+
+<style>
+
+.continer {
+  height: 25rem;
+  width:12rem;
+  background-color: aqua;
+}
+.continer img{
+  height: auto;
+  width:50%;
+  border-radius: 50%;
+}
+</style>
