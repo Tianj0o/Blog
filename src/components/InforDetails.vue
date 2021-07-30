978b210fc3d1cdf17d33937aa917e7309b4b1682
Tianj0o <2576099053@qq.com>
2021-07-30 23:59:43 +0800

    add

--- src/components/InforDetails.vue
+++ src/components/InforDetails.vue
@@ -1,30 +1,59 @@
 <template>
- <div id="InforDetails">
-   <div class="continer">
-     <img src="../assets/avatar.jpg" alt="个人头像">
-   </div>
- </div>
+  <div id="InforDetails">
+    <div class="continer">
+      <img class="avatar" src="../assets/avatar.jpg" alt="个人头像" />
+      <div>
+        <span>一个无聊的人</span>
+        <p>代码、电影、吉他、乒乓球</p>
+      </div>
+      <p>{{sentence.content}}</p>
+    </div>
+  </div>
 </template>
 <script>
-import { defineComponent } from "@vue/runtime-core";
 
-
-
+import { defineComponent, onMounted, reactive } from "@vue/runtime-core";
+import axios from 'axios'
 export default defineComponent({
+setup(){
+  const sentence = reactive({
+    content:String,
+    origin:String
+  })
+  onMounted(()=>{
+    axios.get('https://api.xygeng.cn/one').then((res)=>{
+      sentence.content = res.data.data.content
+      console.log(sentence.content)
+    })
   
-})
+  })
+  return {
+    sentence
+  }
+}
+});
 </script>
 
 <style>
-
 .continer {
   height: 25rem;
-  width:12rem;
+  width: 12rem;
   background-color: aqua;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  flex-direction: column;
 }
-.continer img{
+.continer img {
   height: auto;
-  width:50%;
+  width: 50%;
   border-radius: 50%;
+  transition: width 1.5s;
+  transition: transform 1.5s;
+}
+.avatar:hover {
+  height: auto;
+  width: 60%;
+  transform: rotate(720deg);
 }
 </style>
